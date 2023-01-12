cc7a512d067e11e92232957dd18fbdca937f2e42
avbans <avbans93@gmail.com>
2023-01-12 14:32:00 -0800

    1/12/23 AB: Cleaned script up

--- 02_Processing/1_master.R
+++ 02_Processing/1_master.R
@@ -1,4 +1,2 @@
 source("02_Processing\\3_processing.R")
 source("02_Processing\\4_graphs.R")
-
-  
--- 02_Processing/3_processing.R
+++ 02_Processing/3_processing.R
@@ -1,26 +1,25 @@
 #THIS SCRIPT CLEANS DATA UP
 library(tidyverse)
-#import txt file and remove junk rows 
+library(lubridate)
+
+#IMPORTS RAW TXT FILE AND DELETES JUNK ROWS 
 rain_raw <- read_csv(
   "C:\\Users\\Alex's computer\\Desktop\\Portland Rain\\01_Input\\gresham_rain.txt",
                      skip = 8)
 rain_raw<-rain_raw[-c(1:2),] 
 
-#separate day, month, year and total tips
+#COMPILES DAY AND TIPS FROM RAW DATA 
 rain<-rain_raw%>%separate(`Daily  Hourly data -->`,
                           c("day",
                             "month",
                             "year", 
-                            "total"))
-#combine day, month, and year and setting as a date 
-rain<-rain%>%unite('day',day:year,sep="-")
-library(lubridate)
+                            "total_tips"))%>%
+  unite('day',day:year,sep="-")
+
+#READ DATE CORRECTLY 
 rain$day<-dmy(rain$day)
 
-#rename total to tips total 
-colnames(rain)[2]="total_tips"
-
-#total rain in inches 
+#CONVERT TIPS TO INCHES
 rain$total_in<-as.numeric(rain$total_tips)*.01
 
 View(rain)
